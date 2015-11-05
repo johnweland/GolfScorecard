59a27542a5fb06c75dd013b7970c7a232da7068c
unknown <jweland@jweland.ics.loc>
2015-11-05 15:03:08 -0600

    Added shared preferences for stroke count persistence for onPause event.

--- app/src/main/java/me/jweland/golfscorecard/MainActivity.java
+++ app/src/main/java/me/jweland/golfscorecard/MainActivity.java
@@ -1,10 +1,16 @@
 package me.jweland.golfscorecard;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
+    private static final String PREFS_FILE = "me.jweland.golfscorecard.preferences";
+    private static final String KEY_STROKECOUNT = "key_strokecount";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
     private Hole[] mHoles = new Hole[18];
     private ListAdapter mListAdapter;
 
@@ -12,15 +18,28 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
 
         //Initialize holes object
         int strokes = 0;
         for(int i = 0; i < mHoles.length; i++) {
+            strokes = mSharedPreferences.getInt(KEY_STROKECOUNT + i, 0);
             mHoles[i] = new Hole("Hole " + (i + 1) + " :", strokes);
         }
         
         mListAdapter = new ListAdapter(this, mHoles);
         setListAdapter(mListAdapter);
         
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        for (int i = 0; i < mHoles.length; i++) {
+            mEditor.putInt(KEY_STROKECOUNT + i, mHoles[i].getStrokeCount());
+        }
+        mEditor.apply();
     }
 }
